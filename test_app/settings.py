f9c1ed5d7f1e9536b0b918072f941228fbf3211f
h0guera <shalyginetc@gmail.com>
2024-11-02 19:00:21 +0300

    settings: create naming conventions variable

--- test_app/settings.py
+++ test_app/settings.py
@@ -47,6 +47,14 @@ class Settings(BaseSettings):
     db_base: str = "admin"
     db_echo: bool = False
 
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
     # Variables for Redis
     redis_host: str = "test_app-redis"
     redis_port: int = 6379
