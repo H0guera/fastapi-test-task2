4fa96e3f3608c26fc1cdbe8830086171a22cd764
h0guera <shalyginetc@gmail.com>
2024-11-05 12:55:39 +0300

    create stuff for creating access and refresh tokens

--- test_app/utils/auth.py
+++ test_app/utils/auth.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta
 from typing import Any
 
 import jwt
 
 from test_app.settings import settings
+
+JWT_TYPE_FIELD = "type"
+ACCESS_JWT_TYPE = "access"
+REFRESH_JWT_TYPE = "refresh"
 
 
 def encode_jwt(
@@ -29,3 +34,28 @@ def decode_jwt(
         key,
         algorithms=[algorithm],
     )
+
+
+def create_jwt(token_type: str, payload: dict[str, Any]) -> str:
+    """Adds type of token to payload and write token."""
+    payload.update({JWT_TYPE_FIELD: token_type})
+    return encode_jwt(payload=payload)
+
+
+def create_access_token(
+    token_data: dict[str, Any],
+    expire_minutes: int | None = settings.auth_jwt.access_token_expire_minutes,
+) -> str:
+    """Adds exp to payload and creates access token."""
+    now = datetime.utcnow()
+    if expire_minutes:
+        token_data.update(
+            exp=now + timedelta(minutes=expire_minutes),
+        )
+    return create_jwt(token_type=ACCESS_JWT_TYPE, payload=token_data)
+
+
+def create_refresh_token(token_data: dict[str, Any]) -> str:
+    """Creates refresh token."""
+    token_data.update(iat=datetime.utcnow())
+    return create_jwt(token_type=REFRESH_JWT_TYPE, payload=token_data)
