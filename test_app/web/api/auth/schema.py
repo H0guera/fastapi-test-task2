9acdce623fd02ab5a3f7b45f972dcbcd5c717a34
h0guera <shalyginetc@gmail.com>
2024-11-05 13:42:43 +0300

    create TokenInfo pydentic model

--- test_app/web/api/auth/schema.py
+++ test_app/web/api/auth/schema.py
@@ -11,3 +11,11 @@ class UserCreate(UserBase):
     """DTO for creating new user model."""
 
     password: str
+
+
+class TokenInfo(BaseModel):
+    """Success login/refresh response model."""
+
+    access_token: str
+    refresh_token: str | None = None
+    token_type: str = "Bearer"
