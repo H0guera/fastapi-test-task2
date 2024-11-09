10bd44f5bca50257e0b4973117218817ff3fff91
h0guera <shalyginetc@gmail.com>
2024-11-09 18:59:15 +0300

    TaskDAO: create get_all_tasks() method

--- test_app/db/dao/task.py
+++ test_app/db/dao/task.py
@@ -1,9 +1,11 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence
 
 from fastapi import Depends
+from sqlalchemy import ScalarResult, select
 
 from test_app.db.dependencies import get_db_session
 from test_app.db.models.tasks import Task
+from test_app.utils.task_status import TaskStatus
 from test_app.web.api.tasks.schema import TaskBase
 
 if TYPE_CHECKING:
@@ -33,3 +35,16 @@ class TaskDAO:
         )
         self.session.add(task)
         return task
+
+    async def get_all_tasks(
+        self,
+        status: TaskStatus | None,
+    ) -> Sequence[Task]:
+        """Gets all tasks with optional filter by status."""
+        stmt = (
+            select(Task)
+            if status is None
+            else select(Task).where(Task.status == status)
+        )
+        result: ScalarResult[Task] = await self.session.scalars(stmt)
+        return list(result.all())
