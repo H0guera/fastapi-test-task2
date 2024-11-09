ec62c06051d141e82629baf3b6c0be590d02d707
h0guera <shalyginetc@gmail.com>
2024-11-09 17:14:23 +0300

    include router of tasks

--- test_app/web/api/router.py
+++ test_app/web/api/router.py
@@ -1,8 +1,9 @@
 from fastapi.routing import APIRouter
 
-from test_app.web.api import auth, docs, monitoring
+from test_app.web.api import auth, docs, monitoring, tasks
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
