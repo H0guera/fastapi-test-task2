959fba9fbc3498b5adc093d691aa62edcaa29463
h0guera <shalyginetc@gmail.com>
2024-11-09 18:59:54 +0300

    create get_all_tasks() view

--- test_app/web/api/tasks/views.py
+++ test_app/web/api/tasks/views.py
@@ -1,6 +1,6 @@
-from typing import Annotated
+from typing import Annotated, Sequence
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from fastapi import status as http_status
 from fastapi.param_functions import Depends
 
@@ -8,6 +8,7 @@ from test_app.db.dao.task import TaskDAO
 from test_app.db.models.tasks import Task
 from test_app.db.models.users import User
 from test_app.services.auth import get_current_auth_user
+from test_app.utils.task_status import TaskStatus
 from test_app.web.api.tasks.schema import TaskBase
 
 router = APIRouter()
@@ -37,15 +38,39 @@ router = APIRouter()
 async def create_task(
     new_task_object: TaskBase,
     task_dao: Annotated[TaskDAO, Depends()],
-    user: User | None = Depends(get_current_auth_user),
+    user: User = Depends(get_current_auth_user),
 ) -> Task:
     """Creates task."""
-    if user is None:
-        raise HTTPException(
-            status_code=http_status.HTTP_401_UNAUTHORIZED,
-            detail="UNAUTHORIZED",
-        )
     return await task_dao.create_task_model(
         create_task=new_task_object,
         user_id=user.id,
     )
+
+
+@router.get(
+    "/",
+    response_model=list[TaskBase],
+    responses={
+        http_status.HTTP_401_UNAUTHORIZED: {
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "UNAUTHORIZED": {
+                            "summary": "Unauthorized.",
+                            "value": {
+                                "detail": "UNAUTHORIZED",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+)
+async def get_all_tasks(
+    task_dao: Annotated[TaskDAO, Depends()],
+    status: TaskStatus | None = None,
+    user: User = Depends(get_current_auth_user),
+) -> Sequence[Task]:
+    """Gets all tasks with optional filter by status."""
+    return await task_dao.get_all_tasks(status=status)
